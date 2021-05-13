934d65f210ac9c2aba6711a2a72ee8d56b3a339d
Julio Cesar <53705234+juliobonon@users.noreply.github.com>
2021-05-13 18:30:03 -0300

    Update rust-cleaner.rs

--- rust-cleaner.rs
+++ rust-cleaner.rs
@@ -1,15 +1,30 @@
 use std::process::Command;
 use std::io::{self, Write};
 
-fn main() {
-    let output = Command::new("sudo")
+// remove all docker images and volumes
+fn dockerclear(){
+    let docker_output = Command::new("docker")
+                                .arg("system")
+                                .arg("prune")
+                                .arg("--all")
+                                .arg("--volumes")
+                                .arg("--force")
+                                .output()
+                                .expect("failed to docker clean");
+
+    io::stdout().write_all(&docker_output.stdout).unwrap();
+    io::stderr().write_all(&docker_output.stderr).unwrap();
+}
+
+// list how many mbs will be cleared, then apt-get clean
+fn aptgetclean(){
+    let output = Command::new("sudo")55
                          .arg("/usr/bin/du")
                          .arg("-sh")
                          .arg("/var/cache/apt")
                          .output()
                          .expect("failed to execute process");
 
-    println!("status: {}", output.status);
     io::stdout().write_all(&output.stdout).unwrap();
     io::stderr().write_all(&output.stderr).unwrap();
 
@@ -21,8 +36,11 @@ fn main() {
                          .output()
                          .expect("failed to execute process");
 
-    println!("status: {}", apt_output.status);
     io::stdout().write_all(&apt_output.stdout).unwrap();
     io::stderr().write_all(&apt_output.stderr).unwrap();
+}
 
+fn main() {
+    aptgetclean();
+    dockerclear();
 }
